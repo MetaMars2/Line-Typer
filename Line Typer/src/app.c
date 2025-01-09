aab66c2e6a3ed19638b4bd5d86c9e9b9a2e824de
MetaMars2 <david.radoslav@htl-braunau.at>
2025-01-09 11:19:16 +0100

    Add new functions and update function signatures in app
    - Added `get_line_size(FILE* file, int line_index)` to `app.c`
    - Added `get_line(FILE* file)` to `app.c`
    - Renamed `enum exitCode` to `enum exit_code` in `app.h`
    - Updated function signatures in `app.h`:
      - `append_line_to_file` now takes `char* file_name`
      - `line_reader` now takes `const char *file_name`
      - `get_line_count` now takes `const char* file_name`
      - Added declaration for `char *get_line(FILE* file)`

--- Line Typer/src/app.c
+++ Line Typer/src/app.c
@@ -70,6 +70,78 @@ int get_line_count(const char* file_name)
     return count;
 }
 
+size_t get_line_size(FILE* file, int line_index)
+{
+    int current_line = 1;
+    size_t size = 0;
+    char ch;
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        if (current_line == line_index)
+        {
+			while (ch != '\n' && ch != EOF)
+			{
+				size++;
+				ch = fgetc(file);
+			}
+        }
+		current_line++;
+    }
+
+    if (current_line < line_index)
+    {
+        fprintf(stderr, "Line number %d does not exist in the file\n", line_index);
+        return -1;
+    }
+
+    return size;
+}
+
+char *get_line(FILE* file)
+{
+	int line_count = get_line_count(file);
+    int choice = 0;
+
+    printf("What line whould you like to edit: \n");
+	fgets(choice, line_count, stdin);
+
+	if (choice > line_count)
+	{
+		fprintf(stderr, "Invalid line number\n");
+		return NULL;
+	}
+
+	fopen(file, "r");
+	if (file == NULL)
+	{
+		return NULL;
+	}
+
+    char ch;
+
+	for (int i = 0; (ch = fgetc(file)) != EOF; i++)
+	{
+		if (ch == (char)i)
+		{
+            size_t size = get_line_size(file, i);
+			char* line = (char*)malloc(size * sizeof(char));
+			if (line == NULL)
+			{
+				fprintf(stderr, "Memory allocation failed\n");
+				return NULL;
+			}
+			fgets(line, size, file);
+			fclose(file);
+			return line;
+		}
+	}
+
+
+	fclose(file);
+    return NULL;
+}
+
 
 int line_reader(const char *file_name)
 {
--- Line Typer/src/app.h
+++ Line Typer/src/app.h
@@ -1,12 +1,13 @@
 #pragma once
 
-enum exitCode
+enum exit_code
 {
 	FAILURE = -1,
 	SUCCESS = 0
 };
 
 int get_line_input(char** line, int size);
-int append_line_to_file(char* line, char* fileName, int size);
-int line_reader();
-int get_line_count(const char* fileName);
+int append_line_to_file(char* line, char* file_name, int size);
+int line_reader(const char *file_name);
+int get_line_count(const char* file_name);
+char *get_line(FILE* file);
